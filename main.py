0f65051bb10341268943db81320747e0f11bca23
incnone <incohatus.none+git@gmail.com>
2017-01-17 23:52:01 -0500

    forgot to add these to last commit

--- main.py
+++ main.py
@@ -11,34 +11,28 @@ from necrobot.necrobot import Necrobot
 from necrobot.util import backoff, config, seedgen
 
 
-# This reconnect code stolen from https://gist.github.com/Hornwitser/93aceb86533ed3538b6f
-# noinspection PyShadowingNames
-async def keep_running(client, token):
-    retry = backoff.ExponentialBackoff()
+# Define client events
+def ready_client_events():
+    # Called after the client has successfully logged in
+    @client.event
+    async def on_ready():
+        print('-Logged in---------------')
+        print('User name: {0}'.format(client.user.name))
+        print('User id  : {0}'.format(client.user.id))
+        the_necrobot.post_login_init(config.Config.SERVER_ID)
+        print('-------------------------')
+        print(' ')
 
-    while True:
-        try:
-            await client.login(token)
-        except (discord.HTTPException, aiohttp.ClientError):
-            logging.exception('Exception while logging in.')
-            await asyncio.sleep(retry.delay())
-        else:
-            break
+    # Called whenever a new message is posted in any channel on any server
+    @client.event
+    async def on_message(message):
+        cmd = Command(message)
+        await the_necrobot.execute(cmd)
 
-    while client.is_logged_in:
-        if client.is_closed:
-            # noinspection PyProtectedMember
-            client._closed.clear()
-            client.http.recreate()
-
-        try:
-            await client.connect()
-        except (discord.HTTPException, aiohttp.ClientError, discord.GatewayNotFound, discord.ConnectionClosed,
-                websockets.InvalidHandshake, websockets.WebSocketProtocolError) as e:
-            if isinstance(e, discord.ConnectionClosed) and e.code == 4004:
-                raise       # Do not reconnect on authentication failure
-            logging.exception('Exception while running. Retrying.')
-            await asyncio.sleep(retry.delay())
+    # Called when a new member joins any server
+    @client.event
+    async def on_member_join(member):
+        await the_necrobot.on_member_join(member)
 
 
 if __name__ == "__main__":
@@ -76,10 +70,6 @@ if __name__ == "__main__":
 # Seed the random number generator------------------------
     seedgen.init_seed()
 
-# Create the discord.py Client object and the Necrobot----
-    client = discord.Client()
-    the_necrobot = Necrobot(client)
-
 # Try to get login data from login_info if you didn't find it in bot_config; for back-compatibility
     if config.Config.LOGIN_TOKEN == '':
         login_info = open('data/login_info', 'r')
@@ -88,31 +78,50 @@ if __name__ == "__main__":
         config.Config.SERVER_ID = login_info.readline().rstrip('\n')
         login_info.close()
 
-
-# Define client events------------------------------------
-    # Called after the client has successfully logged in
-    @client.event
-    async def on_ready():
-        print('-Logged in---------------')
-        print('User name: {0}'.format(client.user.name))
-        print('User id  : {0}'.format(client.user.id))
-        the_necrobot.post_login_init(config.Config.SERVER_ID)
-        print('-------------------------')
-        print(' ')
-
-
-    # Called whenever a new message is posted in any channel on any server
-    @client.event
-    async def on_message(message):
-        cmd = Command(message)
-        await the_necrobot.execute(cmd)
-
-
-    # Called when a new member joins any server
-    @client.event
-    async def on_member_join(member):
-        await the_necrobot.on_member_join(member)
-
-
 # Run client---------------------------------------------
-    asyncio.get_event_loop().run_until_complete(keep_running(client, config.Config.LOGIN_TOKEN))
+    retry = backoff.ExponentialBackoff()
+
+    while True:
+        # Create the discord.py Client object and the Necrobot----
+        client = discord.Client()
+        the_necrobot = Necrobot(client)
+        ready_client_events()
+
+        while not client.is_logged_in:
+            try:
+                asyncio.get_event_loop().run_until_complete(client.login(config.Config.LOGIN_TOKEN))
+            except (discord.HTTPException, aiohttp.ClientError):
+                logger.exception('Exception while logging in.')
+                asyncio.get_event_loop().run_until_complete(asyncio.sleep(retry.delay()))
+            else:
+                break
+
+        while client.is_logged_in:
+            if client.is_closed:
+                # noinspection PyProtectedMember
+                client._closed.clear()
+                client.http.recreate()
+
+            try:
+                asyncio.get_event_loop().run_until_complete(client.connect())
+
+            except (discord.HTTPException,
+                    aiohttp.ClientError,
+                    discord.GatewayNotFound,
+                    discord.ConnectionClosed,
+                    websockets.InvalidHandshake,
+                    websockets.WebSocketProtocolError) as e:
+
+                if isinstance(e, discord.ConnectionClosed) and e.code == 4004:
+                    raise       # Do not reconnect on authentication failure
+
+                logger.exception('Exception while running.')
+
+            finally:
+                for task in asyncio.Task.all_tasks():
+                    task.cancel()
+
+                asyncio.get_event_loop().run_until_complete(asyncio.sleep(retry.delay()))
+
+        if the_necrobot.quitting:
+            break
